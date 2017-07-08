95675b6ca8cdf9b1f0ab42f560beb2399d565e11
Nick <nmapsy@gmail.com>
2017-07-07 18:05:41 -0700

    BAMRead: Finished cigar parsing to passing all tests. - Remove untested _to_read_coord() function. - Update comments and documentation. - Comment out all logging print statements.

--- lib/tests.py
+++ lib/tests.py
@@ -56,6 +56,10 @@ class CigarConversionTest(unittest.TestCase):
                              .format(cigar_str, read_coord, ref_coord, result))
                 raise
 
+    def test_basic(self):
+        coord_pairs = ((0, None), (1, 781), (2, 782), (284, 1064), (285, None))
+        self._test_many_coords(781, '284M', 163, 284, coord_pairs)
+
     def test_insertion(self):
         coord_pairs = ((159, 8270), (160, None), (168, None), (169, 8271))
         self._test_many_coords(8112, '159M9I115M', 99, 283, coord_pairs)
@@ -73,7 +77,7 @@ class CigarConversionTest(unittest.TestCase):
         self._test_many_coords(6274, '255M12S', 163, 267, coord_pairs)
 
     def test_reverse(self):
-        coord_pairs = ((1, 6307), (2, 6306), (3, 6305), (284, 6022))
+        coord_pairs = ((1, 6307), (2, 6306), (3, 6305), (286, 6022))
         self._test_many_coords(6022, '286M', 83, 286, coord_pairs)
 
     def test_reverse_insertion(self):
@@ -83,6 +87,35 @@ class CigarConversionTest(unittest.TestCase):
     def test_reverse_deletion(self):
         coord_pairs = ((1, 3123), (2, 3122), (17, 3107), (18, 3105))
         self._test_many_coords(2840, '266M1D17M', 83, 283, coord_pairs)
+
+    def test_reverse_deletion_toy(self):
+        coord_pairs = ((1, 1300), (100, 1201), (101, 1100), (200, 1001))
+        self._test_many_coords(1001, '100M100D100M', 83, 200, coord_pairs)
+
+    # The following are taken from Figure 1 of Li et al. 2009 which introduced the SAM format.
+    def test_Li_r001p(self):
+        coord_pairs = ((1, 7), (8, 14), (9, None), (10, None), (11, 15), (14, 18), (15, 20))
+        self._test_many_coords(7, '8M2I4M1D3M', 163, 17, coord_pairs)
+
+    def test_Li_r002p(self):
+        coord_pairs = ((1, None), (3, None), (4, 9), (9, 14), (10, None), (11, 15), (14, 18))
+        self._test_many_coords(9, '3S6M1P1I4M', 0, 14, coord_pairs)
+
+    def test_Li_r003p(self):
+        coord_pairs = ((0, None), (1, 9), (6, 14), (7, None))
+        self._test_many_coords(9, '5H6M', 0, 6, coord_pairs)
+
+    def test_Li_r004p(self):
+        coord_pairs = ((1, 16), (6, 21), (7, 36), (11, 40))
+        self._test_many_coords(16, '6M14N5M', 0, 11, coord_pairs)
+
+    def test_Li_r003n(self):
+        coord_pairs = ((1, 33), (5, 29))
+        self._test_many_coords(29, '6H5M', 16, 5, coord_pairs)
+
+    def test_Li_r001n(self):
+        coord_pairs = ((1, 45), (9, 37))
+        self._test_many_coords(37, '9M', 83, 9, coord_pairs)
 
 
 def logging_setup():
