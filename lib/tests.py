2056a0da1ceaccbb6354cc1c5eeecdfe664c9049
Nick <nmapsy@gmail.com>
2017-07-08 16:15:53 -0700

    BAMRead: Move tests for BAMRead._get_indels() to unittest framework. - Took tests from tests/unit.test.py and added to lib/tests.py.

--- lib/tests.py
+++ lib/tests.py
@@ -47,6 +47,7 @@ class CigarConversionTest(unittest.TestCase):
         read = pyBamParser.read.BAMRead('12345678901234567890123456789012', None)
         cigar_list = cigar.split_cigar(cigar_str)
         blocks = read._get_contiguous_blocks(pos, cigar_list, flags & 16, readlen)
+        logging.info(blocks)
         for read_coord, ref_coord in coord_pairs:
             result = read._to_ref_coord(blocks, read_coord)
             try:
@@ -116,6 +117,113 @@ class CigarConversionTest(unittest.TestCase):
     def test_Li_r001n(self):
         coord_pairs = ((1, 45), (9, 37))
         self._test_many_coords(37, '9M', 83, 9, coord_pairs)
+
+
+class CigarGetIndelsTest(unittest.TestCase):
+
+    def _test_read(self, pos, cigar_str, flags, readlen, expected):
+        read = pyBamParser.read.BAMRead('12345678901234567890123456789012', None)
+        cigar_list = cigar.split_cigar(cigar_str)
+        blocks = read._get_contiguous_blocks(pos, cigar_list, flags & 16, readlen)
+        logging.info(blocks)
+        indels = read._get_indels(blocks, flags & 16)
+        self.assertEqual(indels, expected)
+
+    def test_M(self):
+        indels = ([], [])
+        self._test_read(1, '251M', 99, 251, indels)
+
+    def test_SM(self):
+        indels = ([], [])
+        self._test_read(1, '3S248M', 99, 251, indels)
+
+    def test_MS(self):
+        indels = ([], [])
+        self._test_read(111, '205M46S', 163, 251, indels)
+
+    def test_MIMa(self):
+        indels = ([(5, 2)], [])
+        self._test_read(2, '4M2I245M', 147, 251, indels)
+
+    def test_MIMb(self):
+        indels = ([(310, 1)], [])
+        self._test_read(199, '112M1I138M', 83, 251, indels)
+
+    def test_MDMa(self):
+        indels = ([], [(3106, 1)])
+        self._test_read(2941, '166M1D85M', 99, 251, indels)
+
+    def test_MDMb(self):
+        indels = ([], [(1900, 1)])
+        self._test_read(1785, '116M1D135M', 83, 251, indels)
+
+    def test_SMS(self):
+        indels = ([], [])
+        self._test_read(1, '10S156M85S', 163, 251, indels)
+
+    def test_SMDM(self):
+        indels = ([], [(2528, 1)])
+        self._test_read(2526, '11S3M1D237M', 83, 251, indels)
+
+    def test_SMIM(self):
+        indels = ([(556, 3)], [])
+        self._test_read(554, '38S3M3I207M', 83, 251, indels)
+
+    def test_MDMS(self):
+        indels = ([], [(3106, 1)])
+        self._test_read(2883, '224M1D26M1S', 163, 251, indels)
+
+    def test_MIMS(self):
+        indels = ([(4269, 1)], [])
+        self._test_read(4099, '171M1I11M68S', 99, 251, indels)
+
+    def test_MIMDM(self):
+        indels = ([(14880, 2)], [(14883, 2)])
+        self._test_read(14640, '241M2I3M2D5M', 163, 251, indels)
+
+    def test_MIMIM(self):
+        indels = ([(16569, 39), (16573, 2)], [])
+        self._test_read(16365, '205M39I4M2I1M', 83, 251, indels)
+
+    def test_MIMDMSa(self):
+        indels = ([(10172, 1)], [(10175, 2)])
+        self._test_read(9931, '242M1I3M2D2M3S', 99, 251, indels)
+
+    def test_MIMDMSb(self):
+        indels = ([(16568, 62)], [(16570, 2)])
+        self._test_read(16390, '179M62I2M2D2M6S', 99, 251, indels)
+
+    def test_SMDMDM(self):
+        indels = ([], [(6806, 1), (6808, 1)])
+        self._test_read(6800, '3S7M1D1M1D240M', 147, 251, indels)
+
+    def test_SMIMIM(self):
+        indels = ([(5979, 1), (5980, 1)], [])
+        self._test_read(5975, '10S5M1I1M1I233M', 83, 251, indels)
+
+    def test_MIMIMS(self):
+        indels = ([(11348, 3), (11354, 2)], [])
+        self._test_read(11127, '222M3I6M2I5M13S', 163, 251, indels)
+
+    def test_SMDMIM(self):
+        indels = ([(6110, 1)], [(6120, 1)])
+        self._test_read(6109, '66S2M1D9M1I173M', 83, 251, indels)
+
+    def test_SMIMDM(self):
+        indels = ([(7604, 1)], [(7605, 2)])
+        self._test_read(7603, '12S2M1I1M2D235M(7604, 1)', 83, 251, indels)
+
+    def test_MDMIMDMS(self):
+        indels = ([(202, 2)], [(199, 1), (210, 2)])
+        self._test_read(1, '199M1D2M2I8M2D2M38S', 99, 251, indels)
+
+    def test_SMDMDMDM(self):
+        indels = ([], [(10522, 1), (10527, 1), (10530, 1)])
+        self._test_read(10517, '32S6M1D4M1D2M1D207M', 147, 251, indels)
+
+    def test_MIMIMDMS(self):
+        indels = ([(13605, 2), (13614, 1)], [(13615, 2)])
+        self._test_read(13388, '218M2I9M1I1M2D11M9S', 163, 251, indels)
 
 
 def logging_setup():
